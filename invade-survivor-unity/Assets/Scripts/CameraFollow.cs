21cfd6395ebb07ee9c1052241d7514c6d455c820
Hyuk Jin Choi <sc62070@gmail.com>
2022-03-17 00:14:50 +0900

    Main Menu added

--- invade-survivor-unity/Assets/Scripts/CameraFollow.cs
+++ invade-survivor-unity/Assets/Scripts/CameraFollow.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform target;
+    public Vector3 offset;
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate() 
     {
-        
+        transform.position = target.position+offset;
     }
 }
