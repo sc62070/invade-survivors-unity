dcab7d82828c0b1c3db747d3d34dc400a4c6cca2
Hyuk Jin Choi <sc62070@gmail.com>
2022-03-16 13:39:49 +0900

    player and joystick position fixed

--- invade-survivor-unity/Assets/Scripts/Player.cs
+++ invade-survivor-unity/Assets/Scripts/Player.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public MovementJoystick movementJoystick;
+    public float playerSpeed;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();    
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(movementJoystick.joystickVec.y !=0)
+        {
+            rb.velocity = new Vector2(movementJoystick.joystickVec.x * playerSpeed, movementJoystick.joystickVec.y * playerSpeed);
+        }
+
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
     }
 }
