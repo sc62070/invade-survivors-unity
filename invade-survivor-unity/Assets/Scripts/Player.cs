a28f1e8e561b8cd86ecb7c90f8a102b43c91b05f
Hyuk Jin Choi <sc62070@gmail.com>
2022-03-18 20:31:25 +0900

    bullet added

--- invade-survivor-unity/Assets/Scripts/Player.cs
+++ invade-survivor-unity/Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour{
 
+    public GameObject bullet;
+
     private Rigidbody2D myBody;
 
     public float speed;
@@ -14,15 +16,31 @@ public class Player : MonoBehaviour{
         myBody = GetComponent<Rigidbody2D>();
     }
 
+    private void Update() 
+    {
+            Rotation();
+            //Shoot Function
+            if(Input.GetMouseButtonDown(0))
+            Instantiate(bullet, transform.position, Quaternion.identity);
+    }
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Movement();
+    }
+
+    void Rotation() {
+        Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 10 * Time.deltaTime);
     }
 
     void Movement()
     {
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveVelocity = moveInput.normalized * speed;
+        myBody.MovePosition(myBody.position + moveVelocity * Time.fixedDeltaTime);
     }
 }
